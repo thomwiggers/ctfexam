20259244789b91e27a307ec90fafb6b8d322527d
Thom Wiggers <thom@thomwiggers.nl>
2020-05-25 11:55:28 +0200

    Validate student numbemrs

--- users/models.py
+++ users/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
+
+
+def validate_student_number(student_number):
+    if (numbers := settings.get('VALID_STUDENT_NUMBERS')) is not None:
+        if student_number not in numbers:
+            raise ValidationError("This is not a known student number")
+
 
 class User(AbstractUser):
     REQUIRED_FIELDS = [
@@ -17,4 +26,7 @@ class User(AbstractUser):
         blank=False,
         null=False,
         unique=True,
+        validators=[
+            validate_student_number,
+        ],
     )
