6feef5f6ad68600ecd0a95c4def4c5060748a1d5
Thom Wiggers <thom@thomwiggers.nl>
2020-05-25 18:09:20 +0200

    Add username

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,6 @@ class UserCreationForm(BaseUserCreationForm):
     class Meta(BaseUserCreationForm.Meta):
         model = models.User
         fields = [
-            'first_name', 'last_name', 'email', 'student_number',
-            'password1', 'password2',
+            'username', 'first_name', 'last_name', 'email',
+            'student_number', 'password1', 'password2',
         ]
