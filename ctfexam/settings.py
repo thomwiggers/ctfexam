ff637642d0572a301ab2d9e0aa7d4b5f077c35e0
Thom Wiggers <thom@thomwiggers.nl>
2021-05-18 09:38:34 +0200

    Django deprecation update

--- ctfexam/settings.py
+++ ctfexam/settings.py
@@ -141,6 +141,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
